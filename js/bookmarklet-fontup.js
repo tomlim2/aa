e6f8d3147ba98183fc8efb0c9ef97613eeb926f3
tomlim2 <tomlim@youns-MacBook-Pro.local>
2017-09-13 08:32:57 -0700

    Drew

--- js/bookmarklet-fontup.js
+++ js/bookmarklet-fontup.js
@@ -32,7 +32,16 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
 
-        
+        var p=document.getElementsByTagName('*');
+
+        for(i=0;i<p.length;i++){
+          if(p[i].style.fontSize){
+            var s=parseInt(p[i].style.fontSize.replace("px",""));
+          } else {
+              var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+
+
+
 
     })();
 
