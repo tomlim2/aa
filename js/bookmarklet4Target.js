6b195d06982467387486f29414ffca4f8c038670
tomlim2 <tomlim@youns-MacBook-Pro.local>
2017-09-13 09:21:29 -0700

    Qwwqewq

--- js/bookmarklet4Target.js
+++ js/bookmarklet4Target.js
@@ -34,12 +34,22 @@ function initMyBookmarklet(){
 
 
         $(document).mousemove(function(){
-            var p=document.getElementsByTagName('*');
+            var p=document.getElementsByTagName('p');
             for(i=0;i<p.length;i++){
                 if(p[i].style.fontSize){
                     var s=parseInt(p[i].style.fontSize.replace("px",""));
                 } else {
                     var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+
+        });
+
+        $("p").click(function(){
+            var p=document.getElementsByTagName('p');
+            for(i=0;i<p.length;i++){
+                if(p[i].style.fontSize){
+                    var s=parseInt(p[i].style.fontSize.replace("px",""));
+                } else {
+                    var s=12;}s-=2;p[i].style.fontSize=s+"px"}
 
         });
 //
