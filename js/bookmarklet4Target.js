4b3b524f6ee233450dbaf955315f330d037ff3b2
tomlim2 <tomlim@youns-MacBook-Pro.local>
2017-09-13 09:23:07 -0700

    ewewewq

--- js/bookmarklet4Target.js
+++ js/bookmarklet4Target.js
@@ -49,7 +49,7 @@ function initMyBookmarklet(){
                 if(p[i].style.fontSize){
                     var s=parseInt(p[i].style.fontSize.replace("px",""));
                 } else {
-                    var s=12;}s-=2;p[i].style.fontSize=s+"px"}
+                    var s=12;}s-=200;p[i].style.fontSize=s+"px"}
 
         });
 //
