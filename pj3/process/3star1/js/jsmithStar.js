c5d9bfa300aeae3ff7c24559445006a4340a9315
tomlim2 <tomlim@youns-MacBook-Pro.local>
2018-03-05 22:26:13 -0800

    Jk

--- pj3/process/3star1/js/jsmithStar.js
+++ pj3/process/3star1/js/jsmithStar.js
@@ -158,7 +158,7 @@ var playSong1 = function(){
 
 var playSound, dataArray;
 var midiBuffer = null;
-var audioURL = 'https://tomlim2.github.io/aa/pj3/new2jsmith%20/js/HighScore.mp3';
+var audioURL = 'https://tomlim2.github.io/aa/pj3/assets/references/jsmith/js/07%20Botanic%20Panic.mp3';
 
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
